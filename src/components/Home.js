ccc78f3e43cf84dc2c744f7f2b012d0a65934f70
Sam Medawatta <sampath.medawatta@gmail.com>
2023-10-15 18:27:19 +1100

    add sidebar, home page buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,38 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
 
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>Home</h3>
-      </header>
+    <div className="container-fluid">
+      <div className="row g-3 my-5 justify-content-center  align-items-center">
+        <div className="col-md-3 p-1">
+          <div className="p-5 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+            <Link to={"/provider"} className="nav-link">
+              <div>
+                <h1 className="fs-2">Provider</h1>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="col-md-3 p-1">
+          <div className="p-5 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+            <Link to={"/broker"} className="nav-link">
+              <div>
+                <h1 className="fs-2">Broker</h1>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="col-md-3 p-1">
+          <div className="p-5 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+            <Link to={"/client"} className="nav-link">
+              <div>
+                <h1 className="fs-2">Client</h1>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
