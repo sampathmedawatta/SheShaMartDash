1d2b569dd4eb1643831f1d02e0b1ef5502213844
Sam Medawatta <sampath.medawatta@gmail.com>
2023-10-16 13:08:18 +1100

    add homepage ui changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,31 +6,26 @@ const Home = () => {
     <div className="container-fluid">
       <div className="row g-3 my-5 justify-content-center  align-items-center">
         <div className="col-md-3 p-1">
-          <div className="p-5 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
             <Link to={"/provider"} className="nav-link">
-              <div>
+             <div className="provider shadow-sm d-flex justify-content-around align-items-center">
                 <h1 className="fs-2">Provider</h1>
               </div>
             </Link>
-          </div>
+          
         </div>
         <div className="col-md-3 p-1">
-          <div className="p-5 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
             <Link to={"/broker"} className="nav-link">
-              <div>
+              <div className="broker shadow-sm d-flex justify-content-around align-items-center">
                 <h1 className="fs-2">Broker</h1>
               </div>
             </Link>
-          </div>
         </div>
         <div className="col-md-3 p-1">
-          <div className="p-5 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
             <Link to={"/client"} className="nav-link">
-              <div>
-                <h1 className="fs-2">Client</h1>
+              <div className="client shadow-sm d-flex justify-content-around align-items-center">
+                <h1 className="fs-2 white">Client</h1>
               </div>
             </Link>
-          </div>
         </div>
       </div>
     </div>
