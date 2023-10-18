f7035a790e52af5659b4ac5c4284e6b6c0b8fa6f
“Chankami” <chankami91@gmail.com>
2023-10-18 17:09:46 +1100

    sensor

--- src/components/SensorQuery.js
+++ src/components/SensorQuery.js
@@ -8,6 +8,7 @@ import SensorService from "../services/sensor.service";
 const SensorQuery = () => {
   const [showAlert, setShowAlert] = useState(false);
   const [jsonPayload, setJsonPayload] = useState("");
+  const [sensorData, setSensorData] = useState([]);
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://unpkg.com/@triply/yasgui/build/yasgui.min.js";
@@ -18,6 +19,8 @@ const SensorQuery = () => {
       const yasgui = new Yasgui(document.getElementById("yasgui"), {
         requestConfig: { endpoint: "http://136.186.108.239:4001/sparql" },
         copyEndpointOnNewTab: true,
+        resizeable: true,
+        tabSize:2, 
       });
       document.querySelector(".yasr").style.display = "none";
       document.querySelector(".yasr_btn").style.display = "none";
@@ -30,7 +33,7 @@ const SensorQuery = () => {
       let inputQuery = "";
 
       yasqeInstance.on("query", (instance, req) => {
-        var elements = document.getElementsByClassName("parseErrorIcon");
+      var elements = document.getElementsByClassName("parseErrorIcon");
 
         if (elements.length > 0) {
           console.log("SPARQL compiler Error. Please fix the SPARQL query!");
@@ -38,9 +41,10 @@ const SensorQuery = () => {
         } else {
           inputQuery = instance.getValue();
           const res = SensorService.querySensor(inputQuery);
-          
+          console.log(res.values);
           setJsonPayload(inputQuery)
           setShowAlert(false);
+          setSensorData(res.values);
         }
       });
     };
@@ -50,25 +54,70 @@ const SensorQuery = () => {
     return () => {
       document.body.removeChild(script);
     };
+
   }, []);
+  console.log("leeenrh"+sensorData.length);
+  if (sensorData.length === 0) {
+    console.log("innnnn");
+  }else{
+    console.log("inooo");
+  }
+
+
+    const renderTableHeaders = () => {
+    if (sensorData.length === 0) {
+      return null;
+    }
+
+    const headers = Object.keys(sensorData[0]);
+
+    return (
+      <tr>
+        {headers.map((header, index) => (
+                <th key={index}>
+                {header
+                  .toLowerCase()
+                  .split(' ')
+                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(' ')}
+              </th>
+        ))}
+      </tr>
+    );
+  };
+
+  const renderTableRows = () => {
+    return sensorData.map((data, index) => (
+      <tr key={index}>
+        {Object.values(data).map((value, idx) => (
+          <td key={idx}>{value.value}</td>
+        ))}
+      </tr>
+    ));
+  };
 
   return (
     <div className="container-fluid">
       <ProviderSubMenu></ProviderSubMenu>
       <div className="row">
         <div className="col-12">
-          <div id="yasgui"></div>
+          <br></br>
           <div id="result">
             {showAlert ? (
               <div className="alert alert-danger" role="alert">
                 SPARQL compiler Error. Please fix the SPARQL query!
               </div>
-            ) : (
-              <div className="alert alert-success" role="alert">
-                <pre>{jsonPayload}</pre>
-              </div>
-            )}
-            {}
+            ) : null}
+            <div id="yasgui"></div>
+            <br></br>
+            <div className="table-responsive">
+              <div><h2>Results</h2></div>
+              <table className="table">
+                <thead>{renderTableHeaders()}</thead>
+                <tbody>{renderTableRows()}</tbody>
+              </table>
+            </div>
+          
           </div>
         </div>
       </div>
