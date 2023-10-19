a61cf01b24998610ce41106af824c0b6496f65d5
Sam Medawatta <sampath.medawatta@gmail.com>
2023-10-20 01:02:21 +1100

    add validations to broker registration

--- src/components/Broker.js
+++ src/components/Broker.js
@@ -6,35 +6,83 @@ import { Context } from "../context/context";
 import BrokerSubMenu from "../components/UI/SubMenu/BrokerSubMenu";
 
 const Broker = () => {
-  const registerBroker = useState([]);
 
-  const [rewardAmount, setRewardAmount] = useState("");
-  const handleRewardAmountChange = (event) => {
-    setRewardAmount(event.target.value);
+  const [formData, setFormData] = useState({
+    rewardAmount: "",
+    brokerName: "",
+    endpoint: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  const [brokerName, setBrokerName] = useState("");
-  const handleBrokerNameChange = (event) => {
-    setBrokerName(event.target.value);
-  };
-
-  const [endpoint, setEndpoint] = useState("");
-  const handleEndpointChange = (event) => {
-    setEndpoint(event.target.value);
-  };
-
-  const brokerRegister = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    const validationErrors = {};
+    if (!formData.rewardAmount) {
+      validationErrors.rewardAmount = "Reward amount is required";
+    }
 
-    const params = {
-      rewardAmount: +rewardAmount,
-      brokerName: brokerName,
-      endpoint: endpoint,
-    };
+    if (!formData.brokerName.trim()) {
+      validationErrors.brokerName = "Broker name is required";
+    }
 
-    registerBroker = BrokerService.registerBroker(params);
+    if (!formData.endpoint.trim()) {
+      validationErrors.endpoint = "Endpoint is required";
+    }
+   
+    setErrors(validationErrors);
 
+    if (Object.keys(validationErrors).length === 0) {
+        const params = {
+          rewardAmount: +formData.rewardAmount,
+          brokerName: formData.brokerName.trim(),
+          endpoint: formData.endpoint.trim(),
+        };
+      const registerBroker = BrokerService.registerBroker(params);
+
+    }
   };
+
+
+  // const registerBroker = useState([]);
+
+  // const [rewardAmount, setRewardAmount] = useState("");
+  // const handleRewardAmountChange = (event) => {
+  //   setRewardAmount(event.target.value);
+  // };
+
+  // const [brokerName, setBrokerName] = useState("");
+  // const handleBrokerNameChange = (event) => {
+  //   setBrokerName(event.target.value);
+  // };
+
+  // const [endpoint, setEndpoint] = useState("");
+  // const handleEndpointChange = (event) => {
+  //   setEndpoint(event.target.value);
+  // };
+
+  // const brokerRegister = (e) => {
+
+  //    var forms = document.querySelectorAll(".needs-validation");
+  //   e.preventDefault();
+
+  //   const params = {
+  //     rewardAmount: +rewardAmount,
+  //     brokerName: brokerName,
+  //     endpoint: endpoint,
+  //   };
+
+  //   registerBroker = BrokerService.registerBroker(params);
+
+  // };
 
   return (
     <div>
@@ -48,57 +96,58 @@ const Broker = () => {
         <div className="row">
           <div className="col-12">
             <div className="card">
-              
               <h3>Register Broker</h3>
               <br />
-              <div className="form-group">
-                <label htmlFor="rewardAmount">Reward Amount</label>
-                <input
-                  type="number"
-                  min={0}
-                  id="rewardAmount"
-                  className="form-control"
-                  name="rewardAmount"
-                  onChange={handleRewardAmountChange}
-                  value={rewardAmount}
-                  autoComplete="off"
-                  required
-                />{" "}
-              </div>
-              <div className="form-group">
-                <label htmlFor="brokerName">Broker Name </label>
-                <input
-                  type="text"
-                  id="brokerName"
-                  className="form-control"
-                  name="brokerName"
-                  onChange={handleBrokerNameChange}
-                  value={brokerName}
-                  autoComplete="off"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="endpoint">Endpoint </label>
-                <input
-                  type="text"
-                  id="endpoint"
-                  className="form-control"
-                  name="endpoint"
-                  onChange={handleEndpointChange}
-                  value={endpoint}
-                  autoComplete="off"
-                  required
-                />
-              </div>
-              <div className="form-group col-3 mt-3">
-                <button
-                  className="btn btn-primary btn-block"
-                  onClick={brokerRegister}
-                >
-                  Click
-                </button>
-              </div>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="rewardAmount">Reward Amount</label>
+                  <input
+                    type="number"
+                    min={0}
+                    id="rewardAmount"
+                    className="form-control"
+                    name="rewardAmount"
+                    onChange={handleChange}
+                    autoComplete="off"
+                  />
+                  {errors.rewardAmount && (
+                    <span className="form-error">{errors.rewardAmount}</span>
+                  )}
+                </div>
+                <div className="form-group">
+                  <label htmlFor="brokerName">Broker Name </label>
+                  <input
+                    type="text"
+                    id="brokerName"
+                    className="form-control"
+                    name="brokerName"
+                    onChange={handleChange}
+                    autoComplete="off"
+                  />
+                  {errors.brokerName && (
+                    <span className="form-error">{errors.brokerName}</span>
+                  )}
+                </div>
+                <div className="form-group">
+                  <label htmlFor="endpoint">Endpoint </label>
+                  <input
+                    type="text"
+                    id="endpoint"
+                    className="form-control"
+                    name="endpoint"
+                    onChange={handleChange}
+                    autoComplete="off"
+                  />
+                  {errors.endpoint && (
+                    <span className="form-error">{errors.endpoint}</span>
+                  )}
+                </div>
+                <div className="form-group col-3 mt-3">
+                  <button type="submit" className="btn btn-primary btn-block">
+                    Click
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
