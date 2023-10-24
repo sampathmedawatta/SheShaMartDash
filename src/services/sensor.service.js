99c2c50602364919aa51b86d4f842bfc1f3125bd
Chankami <Chankami@users.noreply.github.com>
2023-10-24 18:31:46 +1100

    Revert "query"

--- src/services/sensor.service.js
+++ src/services/sensor.service.js
@@ -21,78 +21,22 @@ const getSensors = () => {
     });
 };
 
-/*const querySensor = async(payload) => {
-  /*const params = {
-  query: payload,
-  };
+const querySensor = async(payload) => {
      const response = await axiosInstance
-       .post("/sparql", params, headers);
+       .post("/sparql", payload, headers);
        
        console.log(response);
        return response;
- 
-};*/
 
-const querySensor = (payload) => {
-
-  const params = {
-  
-      "result": true,
-      "values": [
-          {
-              "lat": {
-                  "termType": "Literal",
-                  "value": "-37.821658",
-                  "language": "",
-                  "datatype": {
-                      "termType": "NamedNode",
-                      "value": "http://www.w3.org/2001/XMLSchema#string"
-                  }
-              },
-              "long": {
-                  "termType": "Literal",
-                  "value": "145.03904",
-                  "language": "",
-                  "datatype": {
-                      "termType": "NamedNode",
-                      "value": "http://www.w3.org/2001/XMLSchema#string"
-                  }
-              },
-              "measures": {
-                  "termType": "Literal",
-                  "value": "video",
-                  "language": "",
-                  "datatype": {
-                      "termType": "NamedNode",
-                      "value": "http://www.w3.org/2001/XMLSchema#string"
-                  }
-              },
-              "sensor": {
-                  "termType": "NamedNode",
-                  "value": "test sensor"
-              }
-          }
-      ]
-  }
- /* const params = {
-    query: payload,
-  };
-console.log(params)
-  return axiosInstance
-    .post("/sparql", params, headers)
-    .then((response) => {
-      console.log(response);
-      return response;
-     
-    })
-    .catch((error) => {
-      console.error(error);
-      throw error; // Optionally re-throw the error to be handled later
-    });*/
-    return params
+//   const params = {
+//     query: payload,
+//   };
+//   const tempres = {
+//     result: true,
+//     values: [],
+//   };
+//   return tempres;
 };
-
-
 
 const registerSensor = async (params) => {
   const response = await axiosInstance.post(
