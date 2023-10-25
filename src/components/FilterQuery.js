b1bb92d84e471a76ff290569d0a12624d29204ae
Sam Medawatta <sampath.medawatta@gmail.com>
2023-10-26 08:21:29 +1100

    remove duplicates and clean code in filter page

--- src/components/FilterQuery.js
+++ src/components/FilterQuery.js
@@ -12,12 +12,10 @@ function FilterQuery() {
   const [queryData, setQueryData] = useState([]);
   const [selectedOption, setSelectedOption] = useState("Select");
 
-
-
   const buildSparqlQuery = (searchText, selectedOption) => {
-   // Implement your SPARQL query building logic here based on the search text.
+    // Implement your SPARQL query building logic here based on the search text.
     // Replace "test" with the user's input in the SPARQL query.
-/*
+    /*
     const querySensorType = `SELECT ?sensor ?lat ?long ?measures WHERE {?sensor <http://www.w3.org/ns/sosa/observes> ?observes. ?sensor <http://www.w3.org/ns/sosa/hasFeatureOfInterest> ?location. ?observes <http://www.w3.org/2000/01/rdf-schema#label> ?measures. ?location <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat. ?location <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long. ?observes <http://www.w3.org/2000/01/rdf-schema#label> "${searchText}"}`;
 
     const queryCountry = `{"query":"SELECT ?sensor ?lat ?long ?measures WHERE { ?sensor <http://www.w3.org/ns/sosa/observes> ?observes. ?sensor <http://www.w3.org/ns/sosa/hasFeatureOfInterest> ?location. ?observes <http://www.w3.org/2000/01/rdf-schema#label> ?measures . ?location <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat . ?location <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long . ?country <http://rdfs.co/juso/country> \"${searchText}"}`;
@@ -33,27 +31,26 @@ function FilterQuery() {
     // Base query
     let query = `SELECT ?sensor ?lat ?long ?measures WHERE { ?sensor <http://www.w3.org/ns/sosa/observes> ?observes. ?sensor <http://www.w3.org/ns/sosa/hasFeatureOfInterest> ?location. ?observes <http://www.w3.org/2000/01/rdf-schema#label> ?measures . ?location <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat . ?location <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long . `;
 
-    const option = "SensorType";
-    if (option === "SensorType")
+    if (selectedOption === "SensorType")
       query += `?observes <http://www.w3.org/2000/01/rdf-schema#label> "${searchText}"}`; // By SensorType
-    if (option === "Country")
+    if (selectedOption === "Country")
       query += `?country <http://rdfs.co/juso/country> \"${searchText}"}`; // By Country
-    if (option === "Province")
+    if (selectedOption === "Province")
       query += `?Provenance <http://rdfs.co/juso/Provenance> \"${searchText}"}`; // By Provenance
-    if (option === "City")
+    if (selectedOption === "City")
       query += `?City <http://rdfs.co/juso/City>  \"${searchText}"}`; // By City
-    if (option === "Postcode")
+    if (selectedOption === "Postcode")
       query += `?Postcode <http://rdfs.co/juso/Postcode> \"${searchText}"}`; // By Postcode
-    if (option === "Location")
-      query += `FILTER(xsd:decimal(?long) > \"${searchTextLong1}" && xsd:decimal(?long) < \"${searchTextLong2}" && xsd:decimal(?lat) > \"${searchText}" && xsd:decimal(?lat) < \"${searchText}")}`; // By location
+    if (selectedOption === "Location")
+      query += `FILTER(xsd:decimal(?long) > \"${searchTextLong1}" && xsd:decimal(?long) < \"${searchTextLong2}" && xsd:decimal(?lat) > \"${searchTextLat1}" && xsd:decimal(?lat) < \"${searchTextLat2}")}`; // By location
 
     console.log(query);
 
     SensorService.querySensor(query).then((response) => {
-    if (response.status === 200 && response.data.result === true) {
-     console.log(response);
-         setQueryData(response.data.values);
-     } else {
+      if (response.status === 200 && response.data.result === true) {
+        console.log(response);
+        setQueryData(response.data.values);
+      } else {
         console.log(response);
       }
     });
@@ -63,7 +60,6 @@ function FilterQuery() {
     const query = buildSparqlQuery(searchText, selectedOption); // Pass the selected option
     //setSparqlQuery(query);
   };
-  
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
@@ -72,198 +68,121 @@ function FilterQuery() {
   const renderForm = () => {
     if (selectedOption === "Select") {
       return null;
-    } else if (selectedOption === "SensorType") {
+    } else if (
+      selectedOption === "SensorType" ||
+      selectedOption === "Country" ||
+      selectedOption === "Province" ||
+      selectedOption === "City" ||
+      selectedOption === "Postcode"
+    ) {
       return (
         <form>
-           <div className="form-group">
-            <label htmlFor="sensorType">Enter Sensor Type</label>
+          <div className="form-group">
+            {selectedOption && selectedOption === "SensorType" && (
+              <label htmlFor="sensorType">Enter Sensor Type </label>
+            )}
+            {selectedOption && selectedOption === "Country" && (
+              <label htmlFor="sensorType">Enter Country </label>
+            )}
+            {selectedOption && selectedOption === "Province" && (
+              <label htmlFor="sensorType">Enter Province </label>
+            )}
+            {selectedOption && selectedOption === "City" && (
+              <label htmlFor="sensorType">Enter City </label>
+            )}
+            {selectedOption && selectedOption === "Postcode" && (
+              <label htmlFor="sensorType">Enter Postcode </label>
+            )}
             <input
               type="text"
               id="sensorType"
               name="sensorType"
               className="form-control"
               onChange={(e) => setSearchText(e.target.value)}
-              />
-              <br></br> <br></br>
-               <button
+            />
+            <br></br> <br></br>
+            <button
               type="button"
               className="btn btn-add bi bi-search"
-              onClick={handleSearch} >
+              onClick={handleSearch}
+            >
               Search
             </button>
-              
           </div>
-        </form>
-      );
-    } else if (selectedOption === "Country") {
-      return (
-        <form>
-          <div className="form-group">
-            <label htmlFor="country">Enter Country</label>
-            <input
-              type="text"
-              id="country"
-              name="country"
-              className="form-control"
-              onChange={(e) => setSearchText(e.target.value)}
-              />
-              <br></br> <br></br>
-               <button
-              type="button"
-              className="btn btn-add bi bi-search"
-              onClick={handleSearch} >
-              Search
-            </button>
-              
-          </div>
-          {}
-        </form>
-      );
-    } else if (selectedOption === "Province") {
-      return (
-        <form>
-                    <div className="form-group">
-            <label htmlFor="province">Enter Province</label>
-            <input
-              type="text"
-              id="province"
-              name="province"
-              className="form-control"
-              onChange={(e) => setSearchText(e.target.value)}
-              />
-              <br></br> <br></br>
-               <button
-              type="button"
-              className="btn btn-add bi bi-search"
-              onClick={handleSearch} >
-              Search
-            </button>
-              
-          </div>
-        </form>
-      );
-    } else if (selectedOption === "City") {
-      return (
-        <form>
-                    <div className="form-group">
-            <label htmlFor="city">Enter City</label>
-            <input
-              type="text"
-              id="city"
-              name="city"
-              className="form-control"
-              onChange={(e) => setSearchText(e.target.value)}
-              />
-              <br></br> <br></br>
-               <button
-              type="button"
-              className="btn btn-add bi bi-search"
-              onClick={handleSearch} >
-              Search
-            </button>
-              
-          </div>
-          {}
-        </form>
-      );
-    } else if (selectedOption === "Postcode") {
-      return (
-        <form>
-          <div className="form-group">
-            <label htmlFor="sensorType">Enter Postcode</label>
-            <input
-              type="text"
-              id="postcode"
-              name="postcode"
-              className="form-control"
-              onChange={(e) => setSearchText(e.target.value)}
-              />
-              <br></br> <br></br>
-               <button
-              type="button"
-              className="btn btn-add bi bi-search"
-              onClick={handleSearch} >
-              Search
-            </button>
-              
-          </div>
-          {}
         </form>
       );
     } else if (selectedOption === "Location") {
       return (
-   <form>
-  <div className="form-group">
-    <label htmlFor="sensorType">Latitude</label>
-    <div className="row">
-      <div className="col">
-        <input
-          type="text"
-          id="Lat1"
-          name="Lat1"
-          className="form-control"
-          onChange={(e) => setSearchTextLat1(e.target.value)}
-        />
-      </div>
-      <div className="col">
-        <input
-          type="text"
-          id="Lat2"
-          name="Lat2"
-          className="form-control"
-          onChange={(e) => setSearchTextLat2(e.target.value)}
-        />
-      </div>
-    </div>
-    <br></br>
-    
-    <label htmlFor="longitude">Longitude</label>
-    <div className="row">
-      <div className="col">
-        <input
-          type="text"
-          id="long1"
-          name="long1"
-          className="form-control"
-          onChange={(e) => setSearchTextLong1(e.target.value)}
-        />
-      </div>
-      <div className="col">
-        <input
-          type="text"
-          id="long2"
-          name="long2"
-          className="form-control"
-          onChange={(e) => setSearchTextLong2(e.target.value)}
-        />
-      </div>
-    </div>
-    <br />
-    <br />
-    <button
-      type="button"
-      className="btn btn-add bi bi-search"
-      onClick={handleSearch}
-    >
-      Search
-    </button>
-  </div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="sensorType">Latitude</label>
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  id="Lat1"
+                  name="Lat1"
+                  className="form-control"
+                  onChange={(e) => setSearchTextLat1(e.target.value)}
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  id="Lat2"
+                  name="Lat2"
+                  className="form-control"
+                  onChange={(e) => setSearchTextLat2(e.target.value)}
+                />
+              </div>
+            </div>
+            <br></br>
 
-        
+            <label htmlFor="longitude">Longitude</label>
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  id="long1"
+                  name="long1"
+                  className="form-control"
+                  onChange={(e) => setSearchTextLong1(e.target.value)}
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  id="long2"
+                  name="long2"
+                  className="form-control"
+                  onChange={(e) => setSearchTextLong2(e.target.value)}
+                />
+              </div>
+            </div>
+            <br />
+            <br />
+            <button
+              type="button"
+              className="btn btn-add bi bi-search"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
+          </div>
+
           {}
         </form>
       );
     }
-  
+
     return null;
   };
-  
+
   return (
     <div className="row">
       <div className="col-10 sensor-form">
-      
         <h5>Basic Search !</h5>
-        <br /> 
+        <br />
         <form>
           <div className="form-group">
             <label htmlFor="querytype">Select Filter By</label>
@@ -284,7 +203,7 @@ function FilterQuery() {
             </select>
           </div>
         </form>
-  
+
         {renderForm()}
       </div>
     </div>
